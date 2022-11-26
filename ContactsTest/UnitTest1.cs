e7037b8054724e3f1b1cebbdbca236616bf9d0ab
matthewjercino <matthew@jercinovic.dev>
2022-11-27 01:37:56 +1100

    Add project files.

---
+++ ContactsTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ContactsTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ classes/Contact.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace contactsLib
+{
+    public class Contact
+    {
+        public int cid { get; set; }
+        public string name { get; set; }
+        public string phoneNumber { get; set; }
+    }
+
+    public class Contacts
+    {
+        List<Contact> arr;
+        public void add(Contact contact)
+        {
+            arr.Add(contact);
+        }
+
+        public void delete(int index)
+        {
+            arr.RemoveAt(index);
+        }
+
+        public List<string> convertList()
+        {
+            List<string> arrString = new List<string>();
+            foreach (Contact contact in arr)
+            {
+                arrString.Add(contact.cid + ": " + contact.name + ": " + contact.phoneNumber);
+            }
+            return arrString;
+        }
+    }
+}
