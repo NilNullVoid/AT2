e2308e9cbbe2231fe1bdac9d23192acd6e17682b
matthewjercino <matthew@jercinovic.dev>
2022-12-05 20:05:19 +1100

    foundation for xampp lib

--- contactsLib/Contact2.cs
+++ contactsLib/Contact2.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
-namespace contactsLib
+namespace contactsLib2
 {
+    
     public class Contact
     {
         public int cid { get; set; }
@@ -21,18 +23,26 @@ namespace contactsLib
 
     public class Contacts
     {
-        List<Contact> arr = new List<Contact>();
-        public void add(Contact contact)
+        private async Task<string> MakeApiRequest(string url)
         {
-            arr.Add(contact);
+            HttpClient client = new HttpClient();
+            string response = await client.GetStringAsync(url);
+            return response;
+        }
+        private string ApiServer = "localhost:80";
+        public string add(Contact c)
+        {
+            string url = $"http://{ApiServer}/api/add.php?contact={c.cid},{c.name},{c.phoneNumber}";
+            return MakeApiRequest(url).Result;
         }
 
-        public void delete(int index)
+        public string delete(int cid)
         {
-            arr.RemoveAt(index);
+            string url = $"http://{ApiServer}/api/delete.php?cid={cid}";
+            return MakeApiRequest(url).Result;
         }
 
-        public List<string> convertList()
+        /*public List<string> convertList()
         {
             List<string> arrString = new List<string>();
             foreach (Contact contact in arr)
@@ -40,11 +50,11 @@ namespace contactsLib
                 arrString.Add($"{contact.cid}:{contact.name}:{contact.phoneNumber}");
             }
             return arrString;
-        }
+        }*/
         public string search(int cid)
         {
-            int i = arr.FindIndex(contact => contact.cid == cid);
-            return $"{convertList()[i]} at place {i}";
+            string url = $"http://{ApiServer}/api/search.php?cid={cid}";
+            return MakeApiRequest(url).Result;
         }
     }
 }
---
+++ menu2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using contactsLib2;
+
+namespace menu
+{
+    class Program2
+    {
+        static ContactsLink link = new ContactsLink();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Add record\n2. Delete record\n3. Print\n4. Exit");
+                switch (Console.ReadKey().Key.ToString())
+                {
+                    case "D1":
+                        Console.Clear();
+                        Add();
+                        Console.ReadKey();
+                        continue;
+                    case "D2":
+                        Console.Clear();
+                        Delete();
+                        Console.ReadKey();
+                        continue;
+                    case "D3":
+                        Console.Clear();
+                        PrintRecords();
+                        Console.ReadKey();
+                        continue;
+                    case "D4":
+                        Console.Clear();
+                        Console.WriteLine("Thank you for using the Contacts Test Console.");
+                        break;
+                    default:
+                        continue;
+                }
+                break;
+            }
+        }
+        static void Add()
+        {
+            Console.WriteLine("You've selected to add a new record");
+
+            Console.WriteLine("Please enter the CID of the record: ");
+            int cid = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the Name of the record: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter the Phone Number of the record: ");
+            int phonenumber = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(link.get().add(new Contact(cid, name, phonenumber)));
+        }
+        static void Delete()
+        {
+            Console.WriteLine("You've selected to delete a record");
+            Console.WriteLine("Please enter the position of the record: ");
+            int x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(link.get().delete(x));
+        }
+        static void PrintRecords()
+        {
+            Console.WriteLine("position | cid:name:phonenumber");
+            /*for (int i = 0; i < link.get().convertList().Count; i++)
+            {
+                Console.WriteLine($"{i} | {link.get().convertList()[i]}");
+            }*/
+        }
+    }
+}
